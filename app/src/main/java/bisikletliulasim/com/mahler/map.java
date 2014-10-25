cf6760f473f000dc1cb2745a2ab9ef902899572a
Yigit Guneli <yigiit@gmail.com>
2014-10-25 21:15:33 +0300

    beginning to redesign layout structure

--- app/src/main/java/bisikletliulasim/com/mahler/map.java
+++ app/src/main/java/bisikletliulasim/com/mahler/map.java
@@ -1,25 +1,17 @@
 package bisikletliulasim.com.mahler;
 
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.content.Intent;
 import android.content.IntentSender;
-import android.graphics.Color;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Build;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.text.Layout;
+import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
+
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -28,9 +20,7 @@ import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMap.InfoWindowAdapter;
 import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
@@ -42,26 +32,14 @@ import com.google.gson.JsonParser;
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
 
-import com.readystatesoftware.systembartint.SystemBarTintManager;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
-import com.sothree.slidinguppanel.SlidingUpPanelLayout.LayoutParams;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import android.support.v7.appcompat.BuildConfig;
-
-import java.util.HashMap;
-import java.util.Iterator;
 
 public class map extends FragmentActivity implements
         GooglePlayServicesClient.ConnectionCallbacks,
         GooglePlayServicesClient.OnConnectionFailedListener, LocationListener {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
-    Double active_marker_lat = null;
-    Double active_marker_lon = null;
     public SlidingUpPanelLayout sliding_layout = null;
     public View map_fragment = null;
     private static final String LOG_TAG = "BUPHarita";
@@ -70,6 +48,7 @@ public class map extends FragmentActivity implements
     public LocationClient mLocationClient = null;
     public LocationManager mLocationManager = null;
     Location mostRecentLocation;
+    TextView markerInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,30 +61,11 @@ public class map extends FragmentActivity implements
         setContentView(R.layout.activity_map);
 
         sliding_layout = (SlidingUpPanelLayout) findViewById(R.id.sliding_layout);
-        map_fragment = (View) findViewById(R.id.map);
+        map_fragment = findViewById(R.id.map);
 
         getLocation();
         mLocationClient = new LocationClient(this, this, this);
         setUpMapIfNeeded();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mLocationClient.connect();
-        setUpMapIfNeeded();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mLocationClient.connect();
-        setUpMapIfNeeded();
-    }
-    @Override
-    protected void onStop() {
-        mLocationClient.disconnect();
-        super.onStop();
     }
 
     private void setUpMapIfNeeded() {
@@ -188,20 +148,105 @@ public class map extends FragmentActivity implements
                 JsonParser parser = new JsonParser();
                 final JsonObject properties = (JsonObject) parser.parse(marker.getSnippet());
 
-                String title = properties.get("name").getAsString();
-
                 int px = (int) Utils.dpToPx(Constants.SLIDING_PANEL_HEIGHT, getApplicationContext());
                 sliding_layout.setMinimumHeight(px);
                 sliding_layout.setPanelHeight(px);
 
+                String title = properties.get("name").getAsString();
                 TextView title_text = (TextView) findViewById(R.id.captionView);
                 title_text.setText(title);
+
+                CardView card = (CardView) findViewById(R.id.markerDescriptionCard);
+                if (properties.get("description") != null && properties.get("description").isJsonPrimitive()){
+                    if (!properties.get("description").getAsString().equalsIgnoreCase("")) {
+                        View infobox = findViewById(R.id.description);
+                        ImageView image = (ImageView) infobox.findViewById(R.id.label);
+                        image.setImageResource(android.R.drawable.ic_menu_info_details);
+                        TextView label = (TextView) infobox.findViewById(R.id.info);
+                        label.setText(properties.get("description").getAsString());
+                        label.invalidate();
+                        card.setVisibility(View.VISIBLE);
+                    }
+                }else{
+                    card.setVisibility(View.GONE);
+                }
+                card = (CardView) findViewById(R.id.markerAddressCard);
+                if (properties.get("adres") != null &&
+                    properties.get("adres").isJsonPrimitive() &&
+                    !properties.get("adres").getAsString().equalsIgnoreCase("")
+                        ){
+                    View infobox =  findViewById(R.id.address);
+                    ImageView image = (ImageView) infobox.findViewById(R.id.label);
+                    image.setImageResource(android.R.drawable.ic_dialog_map);
+                    TextView label = (TextView) infobox.findViewById(R.id.info);
+                    label.setText(properties.get("adres").getAsString());
+                    label.invalidate();
+                    card.setVisibility(View.VISIBLE);
+                }else{
+                    card.setVisibility(View.GONE);
+                }
+                card = (CardView) findViewById(R.id.markerPromotionCard);
+                Log.i("BUP", "" + properties.get("kampanya"));
+                if (properties.get("kampanya") != null &&
+                        properties.get("kampanya").isJsonPrimitive()&&
+                        !properties.get("kampanya").getAsString().equalsIgnoreCase("")){
+                    View infobox =  findViewById(R.id.promotion);
+                    ImageView image = (ImageView) infobox.findViewById(R.id.label);
+
+                    image.setImageResource(android.R.drawable.star_big_on);
+                    TextView label = (TextView) infobox.findViewById(R.id.info);
+                    Log.i("BUP", "" + label.getText());
+                    label.setText(properties.get("kampanya").getAsString());
+                    label.invalidate();
+                    Log.i("BUP", "" + label.getText());
+                    card.setVisibility(View.VISIBLE);
+                }else{
+                    card.setVisibility(View.GONE);
+                }
+
+
+
+                TextView labeltext = (TextView) findViewById(R.id.labelWeb);
+                markerInfo = (TextView) findViewById(R.id.markerWeb);
+                if (properties.get("web") != null && properties.get("web").isJsonPrimitive()){
+                    markerInfo.setText(properties.get("web").getAsString());
+                    markerInfo.setVisibility(View.VISIBLE);
+                    labeltext.setVisibility(View.VISIBLE);
+                }else{
+                    labeltext.setVisibility(View.GONE);
+                    markerInfo.setVisibility(View.GONE);
+                }
+
+                labeltext = (TextView) findViewById(R.id.labelTelephone);
+                markerInfo = (TextView) findViewById(R.id.markerTelephone);
+                if (properties.get("telefon") != null && properties.get("telefon").isJsonPrimitive()) {
+                    markerInfo.setText(properties.get("telefon").getAsString());
+                    markerInfo.setVisibility(View.VISIBLE);
+                    labeltext.setVisibility(View.VISIBLE);
+                }else{
+                    labeltext.setVisibility(View.GONE);
+                    markerInfo.setVisibility(View.GONE);
+                }
+                card = (CardView) findViewById(R.id.markerImageCard);
+                if (properties.get("resim") != null && properties.get("resim").isJsonPrimitive()) {
+                    ImageView markerImage = (ImageView) findViewById(R.id.markerImage);
+                    Ion.with(markerImage)
+                            .placeholder(R.drawable.no_image)
+                            .error(R.drawable.no_image)
+                            .resize(960, 600)
+                            .centerCrop()
+                            .load(properties.get("resim").getAsString());
+                    markerImage.setVisibility(View.VISIBLE);
+                    card.setVisibility(View.VISIBLE);
+                }else{
+                    card.setVisibility(View.GONE);
+                }
                 sliding_layout.showPanel();
                 return true;
             }
         });
 
-        Marker marker = mMap.addMarker(
+        mMap.addMarker(
                 new MarkerOptions()
                         .icon(BitmapDescriptorFactory.fromResource(Constants.MARKERS[marker_type]))
                         .position(new LatLng(lat, lon))
@@ -267,6 +312,30 @@ public class map extends FragmentActivity implements
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mLocationClient.connect();
+        setUpMapIfNeeded();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mLocationClient.connect();
+        setUpMapIfNeeded();
+    }
+    @Override
+    protected void onStop() {
+        mLocationClient.disconnect();
+        super.onStop();
+    }
+    @Override
+    protected void onPause() {
+        mLocationClient.disconnect();
+        super.onPause();
     }
 
     private void getLocation() {
